b3a6fe183be751fc3e8226f963066fd4fa84906f
Xunxun Shi <XSHI055@Uottawa.ca>
2018-02-17 16:42:28 -0500

    Fix path to project directory

--- guess.sh
+++ guess.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #docker run -v $1:/tf_files -v $2:/img/guess.jpg  xblaster/tensor-guess sh -c "/usr/bin/python /tf_files/label_image.py /img/guess.jpg"
-docker run -it -v /home/xunxun/tensorflow_image_classifier/images/:/tf_files -v /home/xunxun/Desktop/plus.jpg:/img/guess.jpg xblaster/tensor-guess sh -c "/usr/bin/python /tf_files/label_image.py /img/guess.jpg" | grep 'score'
+docker run -it -v /home/xunxun/Quick-Math/images/:/tf_files -v /home/xunxun/Desktop/plus.jpg:/img/guess.jpg xblaster/tensor-guess sh -c "/usr/bin/python /tf_files/label_image.py /img/guess.jpg" | grep 'score'
