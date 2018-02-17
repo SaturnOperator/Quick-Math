4a3d52986a8b065c868aa5928416a27f254c3faa
Xunxun Shi <XSHI055@Uottawa.ca>
2018-02-17 16:30:59 -0500

    Fix guess.sh

--- guess.sh
+++ guess.sh
@@ -1,1 +1,3 @@
-docker run -v $1:/tf_files -v $2:/img/guess.jpg  xblaster/tensor-guess sh -c "./guess.sh" 2> /dev/null
+#!/bin/bash
+#docker run -v $1:/tf_files -v $2:/img/guess.jpg  xblaster/tensor-guess sh -c "/usr/bin/python /tf_files/label_image.py /img/guess.jpg"
+docker run -it -v /home/xunxun/tensorflow_image_classifier/images/:/tf_files -v /home/xunxun/Desktop/plus.jpg:/img/guess.jpg xblaster/tensor-guess sh -c "/usr/bin/python /tf_files/label_image.py /img/guess.jpg" | grep 'score'
